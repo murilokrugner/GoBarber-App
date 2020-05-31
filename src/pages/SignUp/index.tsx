697ffe11c87d429ba42b403e78c0e606a877de92
murilokrugner <murilokrugner@hotmail.com>
2020-05-31 16:42:31 -0300

    first commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => { console.log('Foi') }}>Entrar</Button>
+
+          </Container>
+        </ScrollView>
+        <BackToSignIn onPress={() => { }}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Fazer login</BackToSignInText>
+        </BackToSignIn>
+      </KeyboardAvoidingView>
+    </>
+  );
+}
+
+export default SignUp;
